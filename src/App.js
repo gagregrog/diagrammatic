a063552ab8f31128b9d38c9d4a4fe9530b1a010e
Robert Reed <robert.mc.reed@gmail.com>
2020-07-20 08:05:52 -0700

    formatting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
 import Diagrammatic from './components/Diagrammatic';
-import {
-  createTreeRecursively,
-  directedGraph as graph,
-} from './lib/util';
+import { createTreeRecursively, directedGraph as graph } from './lib/util';
 
 const tree = createTreeRecursively(6);
 
@@ -16,12 +13,14 @@ function App() {
 
   const props = isTree ? { tree } : { graph };
 
-  const toggle = () => setIsTree(state => !state);
+  const toggle = () => setIsTree((state) => !state);
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={toggle} className="toggle">Switch to {isTree ? 'Graph' : 'Tree'}</button>
+        <button onClick={toggle} className="toggle">
+          Switch to {isTree ? 'Graph' : 'Tree'}
+        </button>
         <Diagrammatic {...props} />
       </header>
     </div>
