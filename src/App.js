20edab5b4a88a689590440409f8206b32a749bd8
Robert Reed <robert.mc.reed@gmail.com>
2020-07-21 10:56:57 -0700

    incorporate dtree into app demo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Diagrammatic from './components/Diagrammatic';
-import { createTreeRecursively, directedGraph as graph } from './lib/util';
+import { createTreeRecursively, directedGraph as graph, loadSampleDecisionTree } from './lib/util';
 
 const tree = createTreeRecursively(6);
+loadSampleDecisionTree().then((dtree) => {
+  console.log(dtree);
+  dataStructures.push({
+    tree: dtree.root,
+    name: 'DTree',
+  });
+});
+
+const dataStructures = [
+  {
+    tree: tree,
+    name: 'Tree',
+  },
+  {
+    graph,
+    name: 'Graph',
+  },
+];
 
 console.log(tree);
 console.log(graph);
 
 function App() {
-  const [isTree, setIsTree] = useState(true);
+  const [data, setData] = useState({ idx: 0, data: dataStructures[0] });
 
-  const props = isTree ? { tree } : { graph };
+  const switchDataStructure = useCallback(() => {
+    let nextIdx = data.idx + 1;
+    if (nextIdx >= dataStructures.length) nextIdx = 0;
 
-  const toggle = () => setIsTree((state) => !state);
+    const nextData = dataStructures[nextIdx];
+
+    setData({ idx: nextIdx, data: nextData });
+  }, [data]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={toggle} className="toggle">
-          Switch to {isTree ? 'Graph' : 'Tree'}
-        </button>
-        <Diagrammatic {...props} />
+        <div className="ds-container">
+          <div className="ds">
+            Current: <span className="ds-current">{data.data.name}</span>
+          </div>
+          <button onClick={switchDataStructure} className="toggle">
+            Switch Data Structures
+          </button>
+        </div>
+        <Diagrammatic {...data.data} />
       </header>
     </div>
   );
