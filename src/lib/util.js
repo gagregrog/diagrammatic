e10018c2de38ab8440374ead4a90b1385c90f36c
Robert Reed <robert.mc.reed@gmail.com>
2020-07-21 10:55:30 -0700

    load test DTree from csv in util

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,23 +1,26 @@
 import faker from 'faker';
 
 import Tree from './datastructures/Tree';
-import SimpleContent from '../components/SimpleContent';
 import Graph from './datastructures/Graph';
+import SimpleContent from '../components/SimpleContent';
+import DecisionTree from './datastructures/DecisionTree';
 
 const nums = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth'];
 const getSomeNums = () => nums.slice(0, Math.floor(Math.random() * (nums.length - 2)) + 2);
 const getSomeWords = () => getSomeNums().map(() => faker.hacker.ingverb());
 
 const getNodeContent = (parentNode) => (word) => {
-  const name = `${parentNode.name}-${word}`; 
+  const name = `${parentNode.name}-${word}`;
   const id = `${name}-${Math.random()}`;
-  
+
   return { id, name, Content: SimpleContent };
 };
 
 const createDescendants = (parentNode) => {
   parentNode.append(...getSomeWords().map(getNodeContent(parentNode)));
-}
+};
+
+// test tree (random)
 
 export const createTreeRecursively = (depth = 1) => {
   const root = new Tree({
@@ -39,18 +42,20 @@ export const createTreeRecursively = (depth = 1) => {
 };
 
 const directed = true;
+
+// test directed graph
 export const directedGraph = new Graph(directed);
 
 const nodes = directedGraph.createNodes(
-  { name: 'zeroth', Content: SimpleContent, },
-  { name: 'first', Content: SimpleContent, },
-  { name: 'second', Content: SimpleContent, },
-  { name: 'third', Content: SimpleContent, },
-  { name: 'fourth', Content: SimpleContent, },
-  { name: 'fifth', Content: SimpleContent, },
-  { name: 'sixth', Content: SimpleContent, },
-  { name: 'seventh', Content: SimpleContent, },
-  { name: 'eighth', Content: SimpleContent, },
+  { name: 'zeroth', Content: SimpleContent },
+  { name: 'first', Content: SimpleContent },
+  { name: 'second', Content: SimpleContent },
+  { name: 'third', Content: SimpleContent },
+  { name: 'fourth', Content: SimpleContent },
+  { name: 'fifth', Content: SimpleContent },
+  { name: 'sixth', Content: SimpleContent },
+  { name: 'seventh', Content: SimpleContent },
+  { name: 'eighth', Content: SimpleContent },
 );
 
 directedGraph.linkNodes(nodes[0], nodes[6]);
@@ -60,3 +65,13 @@ directedGraph.linkNodes(nodes[4], nodes[0], nodes[3], nodes[7], nodes[8]);
 directedGraph.linkNodes(nodes[6], nodes[5]);
 directedGraph.linkNodes(nodes[7], nodes[8]);
 directedGraph.linkNodes(nodes[8], nodes[7]);
+
+// test decision tree from csv
+const FILE = `/data/deployment-options.csv`;
+
+export const loadSampleDecisionTree = async () => {
+  const dtree = new DecisionTree();
+  await dtree.ingest(FILE, { download: true });
+
+  return dtree;
+};
