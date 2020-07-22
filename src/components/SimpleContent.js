06b7fc694a7419cbfee9afcb6ff9cc151d28b083
Robert Reed <robert.mc.reed@gmail.com>
2020-07-21 20:24:47 -0700

    data.option -> data.name -> name

--- src/components/SimpleContent.js
+++ src/components/SimpleContent.js
@@ -2,20 +2,15 @@ import React, { useCallback } from 'react';
 
 import './simple-content.css';
 
-const SimpleContent = ({ handleClick, name, idx, id }) => {
+const SimpleContent = ({ handleClick, name, idx, id, isGraph, data }) => {
   const handleClickButton = useCallback(() => {
-    handleClick(idx !== undefined ? idx : id);
-  }, [handleClick, idx, id]);
-  
-  const parts = name.split('-');
+    handleClick(isGraph ? id : idx);
+  }, [handleClick, idx, id, isGraph]);
 
   return (
     <div className="simple-content-root">
-      <button
-        className="simple-content-btn"
-        onClick={handleClickButton}
-      >
-        {parts[parts.length - 1]}
+      <button className="simple-content-btn" onClick={handleClickButton}>
+        {data?.option || data?.name || name}
       </button>
     </div>
   );
